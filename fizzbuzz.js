004f51c4f7881919480f956e8df42bdff020ca93
calexanderberg <carlalexanderberg@gmail.com>
2022-06-09 14:02:10 +0200

    added fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,21 @@
+const fizzer = (i) => {
+  const fizzData = { 3: "Fizz", 5: "Buzz" }; //This will allow us to only have to edit a signle line to update fizzbuzz
+
+  let output = "";
+
+  for (let v in fizzData) {
+    if (i % v === 0) output += fizzData[v];
+  }
+  if (output === "") output = i;
+
+  return output;
+};
+
+const main = (v) => {
+  for (let i = 1; i <= v; i++) {
+    // I really hate starting at 1...
+    console.log(fizzer(i));
+  }
+};
+
+main(100);
