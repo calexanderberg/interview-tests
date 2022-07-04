6197ae7c72cc3ad0bb89704002a8c14baacb0ea1
calexanderberg <carlalexanderberg@gmail.com>
2022-07-04 13:07:39 +0200

    Finished fizzbuzz script

--- fizzbuzz/fizzbuzz.sh
+++ fizzbuzz/fizzbuzz.sh
@@ -2,17 +2,25 @@
 
 
 echo -e "How many numbers do you want to fizz?"
+read num
 
-for i in {1..15}; do
-  out=""
-    [ ! $i%3 ] && out="Fizz"
-    if ! ((i%5)); then
-        out="${out}Buzz"
-    else
-        out="$i"
-        
-    fi;
-    echo $out
+for ((i = 1; i <= num; i++)); do
+    out=""
+    
+    if (( i % 3 == 0 )); then 
+        out="Fizz" 
+    fi
+    
+    if (( i % 5 == 0 )); then 
+        out="${out}Buzz" 
+    fi
+    
+    if [ "$out" = "" ]; then 
+        out="$i" 
+    fi
+
+    echo -e "$out"
+
 done
 
 
