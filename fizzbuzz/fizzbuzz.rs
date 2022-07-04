f877d8e7197e4289dd10ecd3076c8f600254a158
calexanderberg <carlalexanderberg@gmail.com>
2022-07-04 14:35:03 +0200

    Attempted to get user input in rust

--- fizzbuzz/fizzbuzz.rs
+++ fizzbuzz/fizzbuzz.rs
@@ -1,10 +1,12 @@
 
-
 fn main() {
-  fizzer(15);
+  
+  println!("Enter any number:");
+  // Update later
+  fizzer(50);
 }
 
-fn fizzer(v: u32) {
+fn fizzer(v: i32) {
   for i in 1..v+1 {
     match (i%3, i%5) {
       (0, 0) => println!("FizzBuzz"),
